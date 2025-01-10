c14f8f0eb601668d6c7a41d28efb1f9195233846
seoh77 <seoh7dev@gmail.com>
2025-01-10 14:49:33 +0900

    기본편 #7-3. 접근 제어자 종류

--- 기본편/java-basic/src/access/Speaker.java
+++ 기본편/java-basic/src/access/Speaker.java
@@ -1,5 +1,13 @@
 package access;
 
+/*
+    접근 제어자의 종류
+    - private : 모든 외부 호출을 막는다.
+    - default(package-private) : 같은 패키지 안에서 호출은 허용한다.
+    - protected : 같은 패키지 안에서 호출은 허용한다. 패키지가 달라도 상속 관계의 호출은 허용한다.
+    - public : 모든 외부 호출을 허용한다.
+ */
+
 public class Speaker {
     // private으로 설정했기 때문에 해당 클래스 안에서만 volume 변수에 접근할 수 있다.
     private int volume;
