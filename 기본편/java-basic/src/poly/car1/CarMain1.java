65b68eb38cfeab2c84cda3e9528a0bdd7ef21f3a
seoh77 <seoh7dev@gmail.com>
2025-02-09 16:03:55 +0900

    기본편 #13-5. OCP(Open-Closed Principle) 원칙

--- 기본편/java-basic/src/poly/car1/CarMain1.java
+++ 기본편/java-basic/src/poly/car1/CarMain1.java
@@ -1,5 +1,11 @@
 package poly.car1;
 
+/*
+    OCP(Open-Closed Principle) 원칙
+    - Open for extension : 새로운 기능의 추가나 변경 사항이 생겼을 때, 기존 코드는 확장할 수 있어야 한다.
+    - Closed for modification : 기존의 코드는 수정되지 않아야 한다.
+ */
+
 public class CarMain1 {
 
     public static void main(String[] args) {
@@ -13,6 +19,11 @@ public class CarMain1 {
         // 차량 변경(k3 -> Model3Car)
         Model3Car model3Car = new Model3Car();
         driver.setCar(model3Car);
+        driver.drive();
+
+        // 차량 변경(Model3Car -> newCar)
+        NewCar newCar = new NewCar();
+        driver.setCar(newCar);
         driver.drive();
     }
 }
