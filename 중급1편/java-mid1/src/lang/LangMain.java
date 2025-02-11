1feed7bfc7d4c28e3ba3c98839e2bde294ab9f2c
seoh77 <seoh7dev@gmail.com>
2025-02-12 08:33:59 +0900

    중급1 #2-2.  패키지 소개

---
+++ 중급1편/java-mid1/src/lang/LangMain.java
@@ -0,0 +1,10 @@
+package lang;
+
+//import java.lang.System;    // `java.lang` 패키지는 모든 자바 애플리케이션에 자동으로 임포트된다. 따라서 임포트 구문을 사용하지 않아도 된다.
+
+public class LangMain {
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
