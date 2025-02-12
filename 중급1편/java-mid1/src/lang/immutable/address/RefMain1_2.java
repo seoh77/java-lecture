880d520e7f26c104b3a16ed7419cb27c88b4e74e
seoh77 <seoh7dev@gmail.com>
2025-02-12 14:12:35 +0900

    중급1 #3-2. 공유 참조와 사이드 이펙트

--- 중급1편/java-mid1/src/lang/immutable/address/RefMain1_2.java
+++ 중급1편/java-mid1/src/lang/immutable/address/RefMain1_2.java
@@ -1,17 +1,16 @@
 package lang.immutable.address;
 
-public class RefMain1_1 {
+public class RefMain1_2 {
 
     public static void main(String[] args) {
-        // 참조형 변수는 하나의 인스턴스를 공유할 수 있다.
         Address a = new Address("서울");
-        Address b  = a;
+        Address b = new Address("서울");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
         b.setValue("부산");       // b의 값을 부산으로 변경
         System.out.println("부산 -> b");
-        System.out.println("a = " + a);     // 사이드 이펙트 발생 <- 주된 작업 외에 추가적인 부수 효과를 일으키는 것
-        System.out.println("b = " + b);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);     // b의 주소값만 부산으로 변경
     }
 }
