3165ab169f570f8743f4bac2313fee12caa6117f
seoh77 <seoh7dev@gmail.com>
2025-03-18 14:44:59 +0900

    중급1 #10-3. 예외 처리가 필요한 이유3 - 반환 값으로 예외 처리

---
+++ 중급1편/java-mid1/src/exception/ex1/NetworkServiceV1_2.java
@@ -0,0 +1,29 @@
+package exception.ex1;
+
+public class NetworkServiceV1_2 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV1 client = new NetworkClientV1(address);
+        client.initError(data);
+
+        // 정상흐름 + 예외흐름이 섞여 있어서 이해하기 어렵다.
+        String connectResult = client.connect();
+        if(isError(connectResult)) {
+            System.out.println("[네트워크 오류 발생] 오류 코드: " + connectResult);
+            return;
+        }
+
+        String sendResult = client.send(data);
+        if(isError(sendResult)) {
+            System.out.println("[네트워크 오류 발생] 오류 코드: " + sendResult);
+            return;
+        }
+
+        client.disconnect();
+    }
+
+    private boolean isError(String connectResult) {
+        return !connectResult.equals("success");
+    }
+}
