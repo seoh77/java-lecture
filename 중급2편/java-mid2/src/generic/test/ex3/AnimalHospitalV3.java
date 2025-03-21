3c27881a8afd2bbacbe4d742e8bb595c29536c1e
seoh77 <seoh7dev@gmail.com>
2025-03-21 20:43:05 +0900

    중급2 #3-4. 타입 매개변수 제한4 - 타입 매개변수 제한

---
+++ 중급2편/java-mid2/src/generic/test/ex3/AnimalHospitalV3.java
@@ -0,0 +1,23 @@
+package generic.test.ex3;
+
+import generic.animal.Animal;
+
+// `T extends Animal` : 타입 매개변수 `T`를 `Animal`과 그 자식만 받을 수 있도록 제한을 두는 것
+public class AnimalHospitalV3<T extends Animal> {
+
+    private T animal;
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+
+    public void checkup() {
+        System.out.println("동물 이름: " + animal.getName());
+        System.out.println("동물 크기: " + animal.getSize());
+        animal.sound();
+    }
+
+    public T bigger(T target) {
+        return animal.getSize() > target.getSize() ? animal : target;
+    }
+}
