bf3950b0d555cde06a127c7411247a07087a3d90
seoh77 <seoh7dev@gmail.com>
2025-04-03 15:40:57 +0900

    중급2 #6-8. 문제와 풀이2

---
+++ 중급2편/java-mid2/src/collection/list/test/ex2/ShoppingCart.java
@@ -0,0 +1,29 @@
+package collection.list.test.ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    private List<Item> cart = new ArrayList<>();
+
+    public void addItem(Item item) {
+        cart.add(item);
+    }
+
+    public void displayItems() {
+        System.out.println("장바구니 상품 출력");
+        for (Item item : cart) {
+            System.out.println("상품명: " + item.getName() + ", 합계: " + item.getTotalPrice());
+        }
+        System.out.println("전체 가격 합: " + calculateTotalPrice());
+    }
+
+    private int calculateTotalPrice() {
+        int totalPrice = 0;
+        for (Item item : cart) {
+            totalPrice += item.getTotalPrice();
+        }
+        return totalPrice;
+    }
+}
